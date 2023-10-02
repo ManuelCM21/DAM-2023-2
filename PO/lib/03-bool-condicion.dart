da965255751502d8628c30eef02a767087631af9
ManuelCM21 <manuelchuncamamani@gmail.com>
2023-10-02 11:48:14 -0500

    Proyecto Flutter

---
+++ PO/lib/03-bool-condicion.dart
@@ -0,0 +1,8 @@
+void main2() {
+  bool? isActive;
+  if (isActive == null) {
+    print('isActive es null');
+  } else {
+    print('No es null');
+  }
+}
---
+++ PO/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:po/03-bool-condicion.dart';
+
+void main() {
+  print("Hola Flutter");
+  definirVariables();
+  main2();
+  runApp(const MyApp());
+}
+
+void definirVariables() {
+// Strings
+  final String nombre = 'Tony';
+  final apellido = 'Stark';
+// nombre = 'Peter';
+  print('$nombre $apellido');
+// Números
+  int empleados = 10;
+  double salario = 1856.25;
+  print(empleados);
+  print(salario);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Text( 'Flutter Demo Home Page'),
+    );
+  }
+}
